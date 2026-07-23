2cb4b539843a934f1c2ee187bc259772d99f86a7
LolKekOleg <olegvagin57@gmail.com>
2026-07-23 21:38:41 +0300

    Created ShortURL and Storage structure and some handlers for learning functionality

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,12 +5,48 @@ import (
 	"net/http"
 )
 
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Главная страница")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "About page")
+}
+
+func contactsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Contacts")
+}
+
+func abcHandler(w http.ResponseWriter, r *http.Request) {
+	http.NotFound(w, r)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello world!")
+	if r.Method != http.MethodGet {
+		http.Error(w, "Метод неподдерживается", http.StatusMethodNotAllowed)
+		return
+	}
+	name := r.URL.Query().Get("name")
+	fmt.Println(r.URL.Query())
+	if name != "" {
+		fmt.Fprintf(w, "Привет, %s", name)
+	} else {
+		fmt.Fprintln(w, "Привет, гость")
+	}
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/", mainHandler)
 
-	http.ListenAndServe("localhost: 8080", nil)
+	http.HandleFunc("/about", aboutHandler)
+
+	http.HandleFunc("/contacts", contactsHandler)
+
+	http.HandleFunc("/abc", abcHandler)
+
+	http.HandleFunc("/hello", helloHandler)
+
+	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+		fmt.Println("Ошибка запуска сервера")
+	}
 }
